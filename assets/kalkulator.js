bc2bade1c72659f20fac7be5e8d6bd5a3be2bba9
mangwi10 <mankwiyana@gmail.com>
2022-08-23 00:24:31 +0800

    update kalkulator.js

---
+++ assets/kalkulator.js
@@ -0,0 +1,21 @@
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+}
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit){
+    calculator.displayNumber += digit;
+}
